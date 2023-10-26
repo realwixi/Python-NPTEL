8a5bc29e6e55c0e03edc76d8235603f3d2de9f43
realwixi <98758831+realwixi@users.noreply.github.com>
2023-10-26 12:34:25 +0530

    Create bubble.py

---
+++ bubble.py
@@ -0,0 +1,11 @@
+def bubble(a):
+    n=len(a)
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if(a[j]>a[j+1]):
+                temp=a[j]
+                a[j]=a[j+1]
+                a[j+1]=temp
+a=[5,2,4,67,8,9,10,1]
+bubble(a)
+print(a)
